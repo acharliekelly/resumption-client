da7ab5a1fb51c0c701d606bbe78d77c1f663feae
Charlie <charliekelly31415@gmail.com>
2019-03-12 16:21:34 -0400

    Add default style
    supposed to change display if grunt fails to deploy. 
    so far, only partially effective

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,7 @@ const events = require('./basic/events')
 const auth = require('./auth/events')
 
 $(() => {
+  $('body').css('background-color', '#fff')
   auth.initHandlers()
   events.initHandlers()
 })
