bb6846ab5f127bd460069aa548d78c7128160be2
Charlie <charliekelly31415@gmail.com>
2019-03-14 18:07:31 -0400

    remove unnecessary files

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,7 +4,6 @@ const events = require('./basic/events')
 const auth = require('./auth/events')
 
 $(() => {
-  $('body').css('background-color', '#fff')
   auth.initHandlers()
   events.initHandlers()
 })
