aae7ab7f40a473d6f43ca4ba62d1721cfc2c0f37
Charlie <charliekelly31415@gmail.com>
2019-03-22 16:19:28 -0400

    Fix config urls - extra / at end of production url caused malformed API calls: ..herokuapp.com//sign-up, etc

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://boiling-hollows-87968.herokuapp.com/',
+  production: 'https://boiling-hollows-87968.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
